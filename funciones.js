6616da2b9673afc6de650dafc837a814ea9878f5
FrancoRueta <askjfuhje@hotmail.com>
2021-07-22 10:43:43 -0300

    asd

--- funciones.js
+++ funciones.js
@@ -1,4 +1,22 @@
+
+
+
+console.log(miFuncion(3,7));
+
 function miFuncion(a,b){
-    console.log(a+b);
-    console.log("Hila asdasdqwe")
+    return a + b;
 }
+
+
+
+console.log(miFuncion(2,3));
+
+
+
+
+//Se pueden declarar funciones en variables, son funciones de tipo "expresion."
+let sumar = function(a,b){return a+b};
+
+
+resultado = sumar(1,2);
+console.log(resultado);
