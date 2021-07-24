902c862d07e883a809331908ba77fc2f4196f370
frueta <askjfuhje@hotmail.com>
2021-07-24 17:52:38 -0300

    parte 15

--- 09-clases.js
+++ 09-clases.js
@@ -1,10 +1,13 @@
-
-
-
 class Persona{
+
+    static contadorPersona = 0; //Asociado a la clase.
+
+    static get CINCO_ESTATICO(){ return "5";}
+
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
+        this._id = ++Persona.contadorPersona;
     }
     get nombre(){
         return this._nombre.toUpperCase();
@@ -22,12 +25,28 @@ class Persona{
         this._apellido = apellidoNuevo;
     }
 
+    get id(){
+        return "ID: "+this._id;
+    }
+
     _nombreCompleto(){
-        return this._nombre+' '+this._apellido;
+        return this._nombre+' '+this._apellido+' '+this.id;
     }
 
     get nombreCompleto(){
         return this._nombreCompleto().toUpperCase();
+    }
+
+    toString(){
+        return this.nombreCompleto;
+    }
+
+    static saludar(){
+        console.log("Hola, soy "+this.nombre)
+    }
+
+    static saludar2(persona){
+        console.log("Hola, soy "+persona.nombreCompleto);
     }
 }
 
@@ -57,9 +76,42 @@ class Empleado extends Persona{
     set departamento(departamentoNuevo){
         this._departamento = departamentoNuevo;
     }
+
+    get nombreMasOficio(){
+        return "Nombre: "+super.nombreCompleto+', '+"Departamento: "+this.departamento;
+    }
+
+    get nombreCompleto(){
+        return this.nombreMasOficio;
+    }
 }
 
 
 
 let francoProgramador = new Empleado("franco","rueta","programacion");
-console.log(francoProgramador.nombreCompleto)
+console.log(francoProgramador.nombreMasOficio);
+
+
+console.log(tia.toString());
+console.log(francoProgramador.toString());
+
+
+//console.log(tia.saludar()); no se puede llamar un static desde el objeto, solo de la clase.
+
+//Este anda... raro
+Persona.saludar();
+
+//Este anda mejor aunque no entiendo el uso
+Persona.saludar2(tia);
+
+Empleado.saludar2(francoProgramador);
+
+
+console.log(Persona.contadorPersona);
+console.log(Empleado.contadorPersona);
+
+let ejemploEmp = new Empleado("Pepe","Pepe","Peperia");
+
+console.log(Empleado.contadorPersona);
+console.log(Persona.contadorPersona);
+console.log(tia.email);
